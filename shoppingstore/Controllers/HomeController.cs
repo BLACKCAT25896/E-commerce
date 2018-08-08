27e7f7ec514c7888b0095db46de33b2cf879c3ae
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2018-08-09 00:58:31 +0600

    initial commit

--- shoppingstore/Controllers/HomeController.cs
+++ shoppingstore/Controllers/HomeController.cs
@@ -8,6 +8,12 @@ namespace shoppingstore.Controllers
 {
     public class HomeController : Controller
     {
+        //public JsonResult IsUserExists(string UserName)
+        //{
+        //    //check if any of the UserName matches the UserName specified in the Parameter using the ANY extension method.  
+        //    return Json(!db.Users.Any(x => x.UserName == UserName), JsonRequestBehavior.AllowGet);
+        //}
+
         public ActionResult Index()
         {
             return View();
